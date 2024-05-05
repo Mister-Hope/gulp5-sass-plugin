134537bfdef7ccddd32338b19d34f13fe0f84aa4
Mr.Hope <mister-hope@outlook.com>
2024-05-06 03:02:30 +0800

    chore: update linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,158 +1,22 @@
 // @ts-check
-import js from "@eslint/js";
-import eslintImport from "eslint-plugin-import";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import vitest from "eslint-plugin-vitest";
-import tseslint from "typescript-eslint";
+import hopeConfig, { config, tsParser } from "eslint-config-mister-hope";
 
-export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+export default config(
+  ...hopeConfig,
+
   {
     ignores: ["**/dist/**", "**/node_modules/**", "coverage/**"],
   },
+
   {
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
       parserOptions: {
-        parser: tseslint.parser,
+        parser: tsParser,
         tsconfigDirName: import.meta.dirname,
         project: ["./tsconfig.json"],
       },
     },
   },
-  {
-    files: ["**/*.{cjs,js}"],
-    ...tseslint.configs.disableTypeChecked,
-  },
-  {
-    plugins: {
-      import: eslintImport,
-    },
-    rules: {
-      curly: ["error", "multi", "consistent"],
-      "no-duplicate-imports": "off",
-      "no-unmodified-loop-condition": "error",
-      "padding-line-between-statements": [
-        "error",
-        {
-          blankLine: "always",
-          prev: ["const", "let"],
-          next: ["*"],
-        },
-        {
-          blankLine: "any",
-          prev: ["const", "let"],
-          next: ["const", "let"],
-        },
-        {
-          blankLine: "always",
-          prev: ["*"],
-          next: ["return"],
-        },
-      ],
-      "sort-imports": [
-        "error",
-        {
-          allowSeparatedGroups: false,
-          ignoreDeclarationSort: true,
-        },
-      ],
-
-      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
-      "import/first": "error",
-      "import/newline-after-import": "error",
-      "import/no-commonjs": "error",
-      "import/no-cycle": "error",
-      "import/no-duplicates": ["error", { considerQueryString: true }],
-      "import/no-named-default": "error",
-      "import/order": [
-        "error",
-        {
-          alphabetize: {
-            order: "asc",
-            orderImportKind: "asc",
-          },
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling"],
-            "index",
-            "object",
-          ],
-          "newlines-between": "always",
-        },
-      ],
-    },
-  },
-  {
-    files: ["**/*.ts"],
-    rules: {
-      "@typescript-eslint/consistent-type-imports": "warn",
-      "@typescript-eslint/explicit-function-return-type": [
-        "warn",
-        {
-          allowHigherOrderFunctions: true,
-          allowDirectConstAssertionInArrowFunctions: true,
-          allowTypedFunctionExpressions: true,
-        },
-      ],
-      "@typescript-eslint/naming-convention": [
-        "warn",
-        {
-          selector: "default",
-          format: ["camelCase"],
-          leadingUnderscore: "allowSingleOrDouble",
-          trailingUnderscore: "allow",
-        },
-        {
-          selector: ["variable"],
-          format: ["camelCase", "PascalCase", "UPPER_CASE"],
-          leadingUnderscore: "allowSingleOrDouble",
-          trailingUnderscore: "allowSingleOrDouble",
-        },
-        {
-          selector: ["parameter"],
-          format: ["camelCase", "PascalCase"],
-          leadingUnderscore: "allow",
-          trailingUnderscore: "allow",
-        },
-        {
-          selector: ["property"],
-          format: ["camelCase", "PascalCase", "UPPER_CASE"],
-          leadingUnderscore: "allow",
-          trailingUnderscore: "allow",
-        },
-        {
-          selector: "import",
-          format: ["PascalCase", "camelCase"],
-        },
-        {
-          selector: "typeLike",
-          format: ["PascalCase"],
-        },
-      ],
-      "@typescript-eslint/no-explicit-any": ["warn", { ignoreRestArgs: true }],
-      "@typescript-eslint/no-unsafe-member-access": "warn",
-      "@typescript-eslint/prefer-nullish-coalescing": "warn",
-      "@typescript-eslint/prefer-string-starts-ends-with": [
-        "error",
-        { allowSingleElementEquality: "always" },
-      ],
-    },
-  },
-  {
-    files: ["**/__tests__/**/*.spec.ts"],
-    plugins: {
-      vitest,
-    },
-    rules: {
-      ...vitest.configs.recommended.rules,
-    },
-  },
-
-  eslintPluginPrettierRecommended,
 );
