c46dfa380610e74c3329e54fc7b4b253296511e6
Mister-Hope <mister-hope@outlook.com>
2026-02-08 03:54:59 +0800

    build: bump deps

--- __tests__/__fixtures__/helpers.ts
+++ __tests__/__fixtures__/helpers.ts
@@ -16,4 +16,4 @@ export const createVinyl = (filename: string, contents?: Buffer): Vinyl => {
 };
 
 export const normalizeEOL = (str: Buffer | string): string =>
-  str.toString("utf8").replaceAll("\r\n", "\n");
+  str.toString("utf-8").replaceAll("\r\n", "\n");
